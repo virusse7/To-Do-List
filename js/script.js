a80d8e5fca88c5c1acc480ada2e593242b6cea18
Daniel Wieczorek <danielwieczorek901@gmail.com>
2023-08-04 20:05:22 +0200

    remove tasks

--- js/script.js
+++ js/script.js
@@ -11,27 +11,41 @@
         },
     ];
 
-
-    const render = () => {
-        let htmlString = "";
-        for (const task of tasks) {
-            htmlString += `
-                <li>
-                    ${task.content}
-                </li>
-                `;
-        };
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
 
         render();
-    }
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+                <li>
+                    <button class="js-remove">usuń</button>
+                    ${task.content}
+                </li>
+                `;
+        };
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+    };
+
     const onFormSubmit = (event) => {
         event.preventDefault();
 
@@ -41,7 +55,8 @@
             return;
         };
         addNewTask(newTaskContent);
-    }
+    };
+
     const init = () => {
         render();
 
@@ -49,7 +64,6 @@
 
         form.addEventListener("submit", onFormSubmit);
     };
-
 
     init();
 }
